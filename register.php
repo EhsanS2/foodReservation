d7c152f138d8af8410d03a6ecbf4a69698dc8fa5
EhsanS2 <ehsan.ha@gmail.com>
2019-01-23 15:37:57 +0330

    register page added

---
+++ register.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: e_hatefi
+ * Date: 12/11/2018
+ * Time: 11:16 AM
+ */
+
+require_once "classes.php";
+
+$_VALID = True;
+$text = ""; //for showing a message to user
+if(isset($_POST['user'])) {
+    $db = new dBase();
+    $db->connect();
+    $userName = $_POST['name'];
+    $userId = $_POST['user'];
+    $userPass = $_POST['pass'];
+    $userConfPass = $_POST['confPass'];
+    $userEmail = $_POST['email'];
+    $result = $db->getUser($userId);
+    If(!empty($result)) {
+        $text = $text . "This user is not available, please try something else </br>";
+        $_VALID = False;
+    }
+    if($userPass !== $userConfPass) {
+        $text = $text . "Passwords are not same</br>";
+        $_VALID = False;
+    }
+    if(!filter_var($userEmail, FILTER_VALIDATE_EMAIL)) {
+        $text = $text . "Email address is not valid</br>";
+        $_VALID = False;
+    }
+    if($_VALID) {
+        $db->addUser($userId, $userName, $userPass, 0);
+        $text = "You have successfully registered!";
+        header("refresh:3, url:login.php");
+    }
+
+}
+
+?>
+
+<html>
+<head>
+    <link href="style/css.css" rel="stylesheet">
+</head>
+<body>
+<div id="container">
+    <div id="register">
+        <p>Sign Up</p>
+        <form method="post">
+            <p>Full Name : <input type="text" name="name"></p>
+            <p>Username : <input type="text" name="user" ></p>
+            <p>Password : <input type="password" name="pass" ></p>
+            <p>Confirm Password : <input type="password" name="confPass"></p>
+            <p>Email : <input type="email" name="email"></p>
+            <p><input type="submit" ></p>
+            <p><?php echo $text; ?></p>
+        </form>
+    </div>
+</div>
+</body>
+</html>
